646cf91b8a1e25f442dcc87469605259a94993ec
krishnalogs <mohanbhogi05@gmail.com>
2025-05-24 23:14:51 +0530

    0.1 version

---
+++ prog2.py
@@ -0,0 +1,20 @@
+'''
+x=0
+while (x<100):
+    x+=2
+print(x)
+'''
+
+#even or odd using if-elif
+
+number = float(input("enter a number"))
+
+if number == 0:
+    print ("entered number is 0")
+elif (number % 2 == 0):
+    print("number is even ")
+else:
+    print("number is odd")
+
+# Lists
+
