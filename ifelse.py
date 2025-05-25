f46501848d4d6f9f2b8de79568414329db6bbb9e
krishnalogs <mohanbhogi05@gmail.com>
2025-05-25 11:50:47 +0530

    0.1 version

---
+++ ifelse.py
@@ -0,0 +1,13 @@
+#ifelse, if , nested codes
+#  
+#even or odd using if-elif
+'''
+number = float(input("enter a number"))
+
+if number == 0:
+    print ("entered number is 0")
+elif (number % 2 == 0):
+    print("number is even ")
+else:
+    print("number is odd")
+'''
--- prog2.py
+++ prog2.py
@@ -1,21 +1,26 @@
-'''
-x=0
-while (x<100):
-    x+=2
-print(x)
-'''
-
-#even or odd using if-elif
-
-number = float(input("enter a number"))
-
-if number == 0:
-    print ("entered number is 0")
-elif (number % 2 == 0):
-    print("number is even ")
-else:
-    print("number is odd")
-
 # Lists
-#
+#############################
+L1= [12,787,21,3234,2123,323,67,2332,242,243,455,5,212,23,42,342532,353,211,2456,23,765]
+# print (L1[::2])
+# print (L1[0::3])
+# print (L1[:4:2])
+# print (L1[:5:2])
+############################
+#using sort
+# L1.sort() # it will sort the elements within the same list
+# print (L1)
+# print (L1[::-1]) # to reverse the list and use empty values in both start and end values.
+############################
+#using sorted using L1 list
+# L3 = sorted(L1) # while using sorted we will create a new list as a sorted one and original list will remain same
+# sorted(L1) 
+# print (L3)
+# print (L1)
+############################
+# uisng split how we create lists
+# S1= "please,go,back"
+# #split makes string into many words that words will be convert to list
+# # by default space will be considered as seperated in string
+# l4 = S1.split(sep=',', maxsplit=1) #sep defines what seperator python assume, maxsplit defines howmany times the split will continue
+# print(l4)
 
